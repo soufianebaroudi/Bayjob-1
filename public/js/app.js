742681759aaccdb032ff7d6638e375c50cc31c08
antoinedelahaye <l.a.delahaye0902@gmail.com>
2016-02-11 14:31:24 +0100

    Amelioration Formulaire

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,8 @@ $( document ).ready(function(){
 
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 90,// Creates a dropdown of 15 years to control year
+    yearRange: '1908:1998'
   });
 
 });
