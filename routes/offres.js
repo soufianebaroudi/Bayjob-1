c8d12a45df6af59412e2d2c484d3592f5957289d
BendadaAchraf <bendada.achraf@gmail.com>
2016-02-18 01:55:38 +0000

    Affichage offre  version avant dernier
    offre récupéré de la base

--- routes/offres.js
+++ routes/offres.js
@@ -6,34 +6,23 @@ var router = express.Router();
 router.get('/:idOffres', function(req, res, next) {
 
 
-    models.Offres.findOne({
+    models.Offre.findOne({
         where:{id: req.params.idOffres},
         include:[
             {model: models.Recruteur},
             {model: models.Departement},
             {model: models.Contrat_type},
             {model: models.Niveau_etude},
-            {model: models.Mission},
-            {model: models.Competence}
+            {model: models.Mission_offre},
+            {model: models.Competence_offre}
         ]
-    }).then(function(cv){
+    }).then(function(offre){
         console.log(JSON.stringify(offre));
 
         //Liste des mois
         var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
         res.render('offre', { title: 'offre', offre: offre, mois: mois});
     });
-
-
-
-
-    /*models.CV.findById(req.params.idCv).then(function(cv){
-     models.CV.findById(cv.CandidatId).then(function(candidat){
-     nom =candidat.nom;
-     res.render('cv', { title: 'CV' });
-     });
-
-     });*/
 
 
 });
