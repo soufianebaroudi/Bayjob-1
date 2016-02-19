34bae1118760a0cddf8a1c2a2fee2d7a4d0ee002
landa0902 <l.a.delahaye0902@gmail.com>
2016-02-19 11:19:07 +0100

    Modification du Model et du traitement des données Ajout Candidat

--- models/candidat.js
+++ models/candidat.js
@@ -3,10 +3,8 @@ module.exports = function(sequelize, DataTypes) {
   var Candidat = sequelize.define('Candidat', {
     nom: DataTypes.STRING,
     prenom: DataTypes.STRING,
-    mail: DataTypes.STRING,
     telFixe: DataTypes.STRING,
     telMobile: DataTypes.STRING,
-    mdp: DataTypes.STRING,
     adresse: DataTypes.STRING,
     cp: DataTypes.STRING,
     ville: DataTypes.STRING,
@@ -17,6 +15,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Candidat.hasMany(models.CV);
+        Candidat.belongsTo(models.Utilisateur);
       }
     }
   });
