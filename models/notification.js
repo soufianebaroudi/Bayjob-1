d7c8ba445a65540825b1b7f9cca7b78e487c5898
antoine-lecoustre <a.lecoustre@gmail.com>
2016-02-25 12:57:23 +0100

    Ajout du modèle pour les notifications

---
+++ models/notification.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Notification = sequelize.define('Notification', {
+    utilisateurSession: DataTypes.STRING,
+    accepte:DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Notification.belongsTo(models.CV);
+        Notification.belongsTo(models.Offre);
+      }
+    }
+  });
+  return Notification;
+};
